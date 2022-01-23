482bdf8b0a979c930142cf7c5a7e037f551a4140
nikhil-6631 <70875718+nikhil-6631@users.noreply.github.com>
2022-01-23 09:01:46 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,9 +1,8 @@
-function addUser()
-{
-    
+function addUser() {
+
     user_name = document.getElementById("user_name").value;
-    
+
     localStorage.setItem("user_name", user_name);
-    
-    window.location = "Kwitter_room.html";
+
+    window.location = "kwitter_room.html";
 }
